bb4be75802083ea301abc1cf520ec30fe73de44a
bmquinn <brendan-quinn@northwestern.edu>
2017-06-08 14:52:15 -0500

    Run 'rails generate hyrax:work Image'

---
+++ app/models/image.rb
@@ -0,0 +1,15 @@
+# Generated via
+#  `rails generate hyrax:work Image`
+class Image < ActiveFedora::Base
+  include ::Hyrax::WorkBehavior
+  # This must come after the WorkBehavior because it finalizes the metadata
+  # schema (by adding accepts_nested_attributes)
+  include ::Hyrax::BasicMetadata
+
+  self.indexer = ImageIndexer
+  # Change this to restrict which works can be added as a child.
+  # self.valid_child_concerns = []
+  validates :title, presence: { message: 'Your work must have a title.' }
+
+  self.human_readable_type = 'Image'
+end
