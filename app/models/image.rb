bc06ff64269880c9390a8f50c4ccf18fdea46d9b
Karen Didrickson <karendid@gmail.com>
2017-06-23 09:54:28 -0500

    Adding metadata to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,9 +2,6 @@
 #  `rails generate hyrax:work Image`
 class Image < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
-  # This must come after the WorkBehavior because it finalizes the metadata
-  # schema (by adding accepts_nested_attributes)
-  include ::Hyrax::BasicMetadata
 
   self.indexer = ImageIndexer
   # Change this to restrict which works can be added as a child.
@@ -12,4 +9,64 @@ class Image < ActiveFedora::Base
   validates :title, presence: { message: 'Your work must have a title.' }
 
   self.human_readable_type = 'Image'
+
+  property :abstract, predicate: ::RDF::Vocab::DC.abstract, multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  property :accession_number, predicate: ::RDF::URI('http://id.loc.gov/vocabulary/subjectSchemes/local'), multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :call_number, predicate: ::RDF::Vocab::Bibframe.shelfMark, multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :catalog_key, predicate: ::RDF::URI('http://www.w3.org/2002/07/owl#sameAs'), multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  property :citation, predicate: ::RDF::Vocab::DC.bibliographicCitation, multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  property :contributor_role, predicate: ::RDF::Vocab::DC11.contributor, multiple: true do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :creator_attribution, predicate: ::RDF::Vocab::DC11.creator, multiple: true do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :creator_role, predicate: ::RDF::Vocab::DC11.creator, multiple: true do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :genre, predicate: ::RDF::URI('http://www.europeana.eu/schemas/edm/hasType'), multiple: true do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :physical_description, predicate: ::RDF::Vocab::Bibframe.extent, multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  property :related_url_label, predicate: ::RDF::RDFS.label, multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  property :rights_holder, predicate: ::RDF::Vocab::DC.rightsHolder, multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  property :style_period, predicate: ::RDF::URI('http://purl.org/vra/StylePeriod'), multiple: true do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :technique, predicate: ::RDF::URI('http://purl.org/vra/Technique'), multiple: true do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  # This must come after the WorkBehavior because it finalizes the metadata
+  # schema (by adding accepts_nested_attributes)
+  include ::Hyrax::BasicMetadata
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -2,10 +2,35 @@
 #  `rails generate hyrax:work Image`
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe Image do
-  it { is_expected.to respond_to(:title) }
-  it { is_expected.to respond_to(:depositor) }
+  # Basic Metadata
+  it { is_expected.to respond_to(:based_near) }
+  it { is_expected.to respond_to(:contributor) }
   it { is_expected.to respond_to(:creator) }
+  it { is_expected.to respond_to(:depositor) }
   it { is_expected.to respond_to(:description) }
+  it { is_expected.to respond_to(:keyword) }
+  it { is_expected.to respond_to(:language) }
+  it { is_expected.to respond_to(:license) }
+  it { is_expected.to respond_to(:publisher) }
+  it { is_expected.to respond_to(:related_url) }
   it { is_expected.to respond_to(:rights_statement) }
+  it { is_expected.to respond_to(:title) }
+  # NU Specific
+  it { is_expected.to respond_to(:accession_number) }
+  it { is_expected.to respond_to(:abstract) }
+  it { is_expected.to respond_to(:call_number) }
+  it { is_expected.to respond_to(:catalog_key) }
+  it { is_expected.to respond_to(:citation) }
+  it { is_expected.to respond_to(:creator_role) }
+  it { is_expected.to respond_to(:creator_attribution) }
+  it { is_expected.to respond_to(:contributor_role) }
+  it { is_expected.to respond_to(:genre) }
+  it { is_expected.to respond_to(:physical_description) }
+  it { is_expected.to respond_to(:related_url_label) }
+  it { is_expected.to respond_to(:rights_holder) }
+  it { is_expected.to respond_to(:style_period) }
+  it { is_expected.to respond_to(:technique) }
 end
+# rubocop:enable Metrics/BlockLength
