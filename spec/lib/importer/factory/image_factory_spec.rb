da4632d703c2b63daa969ad4a8ef73dd2d0201ef
bmquinn <brendan-quinn@northwestern.edu>
2017-12-14 12:18:04 -0600

    Use instance spies instead of doubles

--- spec/lib/importer/factory/image_factory_spec.rb
+++ spec/lib/importer/factory/image_factory_spec.rb
@@ -3,7 +3,7 @@ require 'importer'
 
 RSpec.describe Importer::Factory::ImageFactory, :clean do
   let(:factory) { described_class.new(attributes) }
-  let(:actor) { double }
+  let(:actor) { instance_spy('actor') }
   let(:bucket) { 'test-1234' }
   let(:csv_file_key) { 'sample.csv' }
   let(:csv_resource) { Aws::S3::Object.new(client: Aws::S3::Client.new, bucket_name: bucket, key: csv_file_key) }
@@ -26,11 +26,11 @@ RSpec.describe Importer::Factory::ImageFactory, :clean do
 
     context 'for a new image' do
       it 'creates file sets with access controls' do
-        expect(actor).to receive(:create).with(Hyrax::Actors::Environment) do |k|
+        factory.run
+        expect(actor).to have_received(:create).with(Hyrax::Actors::Environment) do |k|
           expect(k.attributes).to include(member_of_collections: [coll])
           expect(k.attributes[:remote_files].first[:url]).to start_with('http://localhost:9000/test-1234/files/coffee.jpg')
         end
-        factory.run
       end
     end
 
@@ -39,11 +39,11 @@ RSpec.describe Importer::Factory::ImageFactory, :clean do
       let(:factory) { described_class.new(attributes.merge(id: work.id), csv_resource) }
 
       it 'creates file sets' do
-        expect(actor).to receive(:update).with(Hyrax::Actors::Environment) do |k|
+        factory.run
+        expect(actor).to have_received(:update).with(Hyrax::Actors::Environment) do |k|
           expect(k.attributes).to include(member_of_collections: [coll])
           expect(k.attributes[:remote_files].first[:url]).to start_with('http://localhost:9000/test-1234/files/coffee.jpg')
         end
-        factory.run
       end
     end
   end
@@ -53,12 +53,10 @@ RSpec.describe Importer::Factory::ImageFactory, :clean do
     let(:factory) { described_class.new(attributes, csv_resource) }
 
     it 'does not create a new collection' do
-      expect(actor).to receive(:create).with(Hyrax::Actors::Environment) do |k|
+      expect { factory.run }.to change { Collection.count }.by(0)
+      expect(actor).to have_received(:create).with(Hyrax::Actors::Environment) do |k|
         expect(k.attributes).to include(member_of_collections: [coll])
       end
-      expect do
-        factory.run
-      end.to change { Collection.count }.by(0)
     end
   end
 end
