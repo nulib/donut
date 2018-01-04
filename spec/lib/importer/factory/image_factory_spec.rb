39ad48c0fa03295a52879b58a248cc27f6552d9c
bmquinn <brendan-quinn@northwestern.edu>
2018-01-04 15:03:43 -0600

    Allow setting admin_set in batch csv

--- spec/lib/importer/factory/image_factory_spec.rb
+++ spec/lib/importer/factory/image_factory_spec.rb
@@ -34,6 +34,17 @@ RSpec.describe Importer::Factory::ImageFactory, :clean do
       end
     end
 
+    context 'with an admin set' do
+      let(:factory) { described_class.new(attributes.merge(admin_set_id: 'batch-test'), csv_resource) }
+
+      it 'can add to admin sets' do
+        factory.run
+        expect(actor).to have_received(:create).with(Hyrax::Actors::Environment) do |k|
+          expect(k.attributes).to include(admin_set_id: 'batch-test')
+        end
+      end
+    end
+
     context 'for an existing image without files' do
       let(:work) { create(:image) }
       let(:factory) { described_class.new(attributes.merge(id: work.id), csv_resource) }
