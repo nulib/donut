ba0af23fb5a13199adfb5904f00f8c7cfae4300d
Chris Syversen <csyversen@gmail.com>
2018-01-30 10:49:27 -0600

    technique now querying authority, specs for new authority

---
+++ spec/views/records/edit_fields/_technique.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'records/edit_fields/_technique.html.erb', type: :view do
+  let(:image) { Image.new }
+  let(:form) { Hyrax::ImageForm.new(image, nil, controller) }
+  let(:form_template) do
+    %(
+      <%= simple_form_for [main_app, @form] do |f| %>
+        <%= render "records/edit_fields/technique", f: f, key: 'technique' %>
+      <% end %>
+    )
+  end
+
+  before do
+    assign(:form, form)
+    render inline: form_template
+  end
+
+  it 'has url for autocomplete service' do
+    expect(rendered).to have_selector('input[data-autocomplete-url="/authorities/search/getty/aat"][data-autocomplete="technique"]')
+  end
+end
