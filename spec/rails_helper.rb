3aa38eccbb9204cfccd9428b42e87ca387a28280
bmquinn <brendan-quinn@northwestern.edu>
2018-02-12 14:49:20 -0600

    Upgrade to latest hyrax-spec and require shared_examples

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'rake'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'hyrax/spec/matchers'
+require 'hyrax/spec/shared_examples'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
