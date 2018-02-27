c02d5c93320dc784cccef100099232dbe5490919
bmquinn <brendan-quinn@northwestern.edu>
2018-02-27 10:32:18 -0600

    Override keyword predicate, update shared example

--- spec/support/shared_examples/a_model_with_nul_core_metadata.rb
+++ spec/support/shared_examples/a_model_with_nul_core_metadata.rb
@@ -1,3 +1,4 @@
 RSpec.shared_examples 'a model with nul core metadata' do
+  it { is_expected.to have_editable_property(:keyword, RDF::Vocab::SCHEMA.keywords) }
   it { is_expected.to have_editable_property(:language, RDF::Vocab::DC11.language) }
 end
