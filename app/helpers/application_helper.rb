843c9aef9eba20d8887228d907cac15b3bcc2529
Michael Klein <mbklein@gmail.com>
2018-03-15 11:34:53 -0500

    Make Karen happy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 module ApplicationHelper
+  def error_collector(error_hash)
+    return 'No errors' if error_hash.empty?
+    error_hash.collect do |field, errors|
+      errors.collect { |error| "#{field}: #{error} "}
+    end.flatten.join('<br/>')
+  end
+
+  def status_context(status)
+    case status
+    when 'error'       then 'danger'
+    when 'complete'    then 'success'
+    when 'initialized' then 'info'
+    when 'processing'  then 'active'
+    else ''
+    end
+  end
+
   def status_label(status)
     status_span_generator status
   end
@@ -6,17 +23,15 @@ module ApplicationHelper
   private
 
     def status_span_generator(status)
-      case status
-      when 'error'
-        content_tag(:span, " #{status.titleize}", class: 'fa fa-exclamation-triangle')
-      when 'complete'
-        content_tag(:span, " #{status.titleize}", class: 'fa fa-check-circle')
-      when 'initialized'
-        content_tag(:span, " #{status.titleize}", class: 'fa fa-info')
-      when 'empty'
-        content_tag(:span, " #{status.titleize}", class: 'fa fa-minus-circle')
-      when 'processing'
-        content_tag(:span, " #{status.titleize}", class: 'fa fa-sync')
+      fa_class = case status
+      when 'error' then 'fa fa-exclamation-triangle'
+      when 'complete' then 'fa fa-check-circle'
+      when 'initialized' then 'fa fa-info'
+      when 'empty' then 'fa fa-minus-circle'
+      when 'processing' then 'fa fa-sync'
+      end
+      content_tag(:span, class: fa_class) do
+        ('&ensp;' + content_tag(:a, status.titleize)).html_safe
       end
     end
 end
