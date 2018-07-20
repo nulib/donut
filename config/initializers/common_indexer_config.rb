b42e35d4130fac52209387160b5e32acc56ce39a
Brendan Quinn <brendan-quinn@northwestern.edu>
2018-07-20 15:43:49 -0500

    Add common_indexer gem and include CommonIndexer in Image model Use released version and update Image#to_common_index Add safe access operator to admin set indexing Remove SQSD; add Kibana to docker dev Move to_common_index to a mixin and add more data Complex mappings Use nulib/common-indexer@mappings Move common indexing to a service to declutter the model Clean up method names in common indexer Add year to mapping Add thumbnail & manifest URIs to mapping Simplify common indexer representative_file index correct url for iiif manifest Use released browse-everything Make sure S3 is loaded before endpoints are stubbed
    Co-authored-by: Brendan Quinn <brendan-quinn@northwestern.edu>

--- config/initializers/common_indexer_config.rb
+++ config/initializers/common_indexer_config.rb
@@ -1,4 +1,3 @@
-::CommonIndexer.config do |config|
+::CommonIndexer.configure do |config|
   config.endpoint = Settings.common_indexer.endpoint
-  config.allowed_keys = Settings.common_indexer.allowed_keys
 end
