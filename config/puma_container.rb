493b300026427699d193b2d245fcf2e43256c2da
Michael B. Klein <mbklein@gmail.com>
2018-11-28 10:42:53 -0600

    Remove /var/log/puma and just log to stdout

--- config/puma_container.rb
+++ config/puma_container.rb
@@ -3,5 +3,4 @@ threads 8, 32
 workers `grep -c processor /proc/cpuinfo`
 port ENV.fetch('PORT') { 3000 }
 pidfile '/var/run/puma/puma.pid'
-stdout_redirect '/var/log/puma/puma.log', '/var/log/puma/puma.log', true
 daemonize false
