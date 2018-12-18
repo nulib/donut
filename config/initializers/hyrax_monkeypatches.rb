f0d0a794993a3d6ec64bf13a8c83a5aa7bdd0f12
Michael Klein <mbklein@gmail.com>
2018-12-18 13:13:39 -0600

    Happy now, cop?

--- config/initializers/hyrax_monkeypatches.rb
+++ config/initializers/hyrax_monkeypatches.rb
@@ -1,7 +1,7 @@
 BrowseEverything::Retriever.class_eval do
   class << self
     alias_method :_can_retrieve?, :can_retrieve?
-    def can_retrieve?(uri, headers)
+    def can_retrieve?(uri, _headers)
       _can_retrieve?(uri)
     end
   end
@@ -9,10 +9,9 @@ end
 
 FileSet.class_eval do
   before_save :ensure_label_present
-  
+
   def ensure_label_present
-    if label.nil? and import_url.present?
-      label = File.basename(URI(import_url).path)
-    end
+    return if label.present? || import_url.nil?
+    self.label = File.basename(URI(import_url).path)
   end
 end
