1927a0527f302760bf54394256af84b19ba40949
Michael Klein <mbklein@gmail.com>
2018-12-19 10:08:11 -0600

    Fix Batch & Browse

--- config/initializers/hyrax_monkeypatches.rb
+++ config/initializers/hyrax_monkeypatches.rb
@@ -8,7 +8,18 @@ BrowseEverything::Retriever.class_eval do
 end
 
 FileSet.class_eval do
+  before_save :decode_import_url
   before_save :ensure_label_present
+
+  def decode_import_url
+    return if import_url.nil?
+    decoded_url = URI.decode(import_url)
+    while import_url != decoded_url
+      Rails.logger.info("Decoding #{import_url}")
+      self.import_url = decoded_url
+      decoded_url = URI.decode(import_url)
+    end
+  end
 
   def ensure_label_present
     return if label.present? || import_url.nil?
