4f1faa268151a59932719d66af6be3a51f400a82
Michael B. Klein <mbklein@gmail.com>
2019-05-13 15:04:32 -0500

    Use the VIPS intermediate format… (#446)
    * Use the VIPS intermediate format to make image scaling and transcoding more memory efficient
    * Update webmock for Ruby 2.6
    * Remove non-functioning jasmine specs

--- app/jobs/create_pyramid_tiff_job.rb
+++ app/jobs/create_pyramid_tiff_job.rb
@@ -6,8 +6,7 @@ class CreatePyramidTiffJob < ApplicationJob
   MAX_PIXELS = 0x3FFF**2
 
   def perform(file_set, file_id, filepath = nil)
-    service = Hyrax::DerivativeService.for(file_set)
-    filename = service.prepare_file(Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id, filepath))
+    filename = vips_file(file_set, file_id, filepath)
     image = Vips::Image.new_from_file(filename)
     scale_factor = MAX_PIXELS / (image.width * image.height).to_f
     image = image.resize(scale_factor) if scale_factor < 1.0
@@ -15,6 +14,17 @@ class CreatePyramidTiffJob < ApplicationJob
   end
 
   private
+
+    def vips_file(file_set, file_id, filepath)
+      service = Hyrax::DerivativeService.for(file_set)
+      filename = service.prepare_file(Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id, filepath))
+      basename = File.basename(filename, File.extname(filename))
+      (File.join(File.dirname(filename), basename) + '.v').tap do |vips_filename|
+        unless File.exist?(vips_filename)
+          Vips::Image.new_from_file(filename).vipssave(vips_filename, strip: true)
+        end
+      end
+    end
 
     def write_tiff(image, file_id)
       output_params = { compression: 'jpeg', Q: 75, tile: true, tile_height: 256, tile_width: 256,
